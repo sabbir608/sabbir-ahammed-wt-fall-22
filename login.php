6f2d9fb5e94cd5bbe67c2af0239b3f61ea7d5372
sabbir608 <101544271+sabbir608@users.noreply.github.com>
2022-10-20 08:04:28 +0600

    lab-2

---
+++ login.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<form action="data.php">
+<head>
+        <link rel="stylesheet" href="Login.css">
+</head>
+
+username:<input type = "text" id="uname" name="username"> 
+<p> </p>
+password:<input type = "password" id="pass" name="pass">
+<p></p>
+<input type="submit" name "btn_login" value="login">
+
+</form>
+</body>
+</form>
